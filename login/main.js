8825861be6fae1c1be46653822d803e937db53df
MOHIT KUMAWAT <kumawatmohit2005204@gmail.com>
2024-11-11 23:30:32 +0530

    Add files via upload

---
+++ login/main.js
@@ -0,0 +1,7 @@
+let a,b;
+document.getElementById("login").onclick = function(){
+ a= document.getElementById("username1").value;
+ b= document.getElementById("password1").value;
+console.log(`Username: ${a}`);
+ console.log(`password: ${b}`);
+} 
